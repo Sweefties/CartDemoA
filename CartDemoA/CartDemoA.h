022afead1b056814a5206781555b2d5938ebe230
Wlad <wlad.djclubing@orange.fr>
2018-01-20 17:16:30 +0100

    Initial Commit

---
+++ CartDemoA/CartDemoA.h
@@ -0,0 +1,19 @@
+//
+//  CartDemoA.h
+//  CartDemoA
+//
+//  Created by Wlad Dicario on 20/01/2018.
+//  Copyright © 2018 socle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CartDemoA.
+FOUNDATION_EXPORT double CartDemoAVersionNumber;
+
+//! Project version string for CartDemoA.
+FOUNDATION_EXPORT const unsigned char CartDemoAVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CartDemoA/PublicHeader.h>
+
+
